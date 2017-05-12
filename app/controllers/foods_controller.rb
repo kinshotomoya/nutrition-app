42503070191d463f16d6e39d2d6ff8358d472761
金正朋也 <mhtnim1023@gmail.com>
2017-05-12 16:06:03 +0900

    edit foods controller and add search action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,5 +3,13 @@ class FoodsController < ApplicationController
   end
 
   def search
+    @foods = Food.where('name LIKE(?)', "%#{search_params}%")
+    # @nutritions = @foods.nutritives
+  end
+
+  private
+
+  def search_params
+    params.require(:food)[:name]
   end
 end
