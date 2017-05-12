0fbec4435696723a1b7ec6969d0946f939588b5b
金正朋也 <mhtnim1023@gmail.com>
2017-05-12 16:04:48 +0900

    add search path on foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users #これを記述していないと、deviseのhelper_methodは使えない！！！！
   resources :tops, only: [:index]
-  resources :foods, only: [:index, :search]
+  resources :foods, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
 end
